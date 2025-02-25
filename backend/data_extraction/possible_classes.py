91e7d8922f5273aa9b351af6e7748b2f72a13566
TayJWise <taylorjwise1@gmail.com>
2025-02-24 20:15:30 -0600

    Adding extraction date to txt

--- backend/data_extraction/possible_classes.py
+++ backend/data_extraction/possible_classes.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 # URL to cs course list
 url = "https://ou-public.courseleaf.com/courses/c_s/"
@@ -14,8 +15,13 @@ for block in soup.find_all("p", class_="courseblocktitle"):
     course_name = block.find("strong").get_text(strip=True)
     courses.append(f"{course_name}")
 
+# Getting time of extraction being done
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 # Write list of all courses to txt file
 with open("backend/data_extraction/data/extracted_classes.txt", "w") as file:
+    # Adding extraction time to top of file
+    file.write(f"Parsed and extracted at time: {current_time}\n\n") 
     for course in courses:
         file.write(f"{course}\n")
 
